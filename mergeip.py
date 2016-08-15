fa58eb846651cca7d4e11648debb2ee23b9a65ff
kaiwang0112006 <kaiwang0112006@yahoo.com>
2016-08-15 09:38:14 +0800

    merge

---
+++ mergeip.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from geopy.geocoders import Nominatim
+import time
+import csv
+
+def findLoc(country,state,city):
+    query=','.join([country,state,city])
+    geolocator = Nominatim()
+    location = geolocator.geocode(query,timeout=None)
+    try:
+        return (location.latitude, location.longitude)
+    except:
+        return ('None','None')
+    
+def do_geocode(country,state,city,recursion=0):
+    try:
+        return findLoc(country,state,city)
+    except:
+        if recursion > 150:
+            return ('None','None')
+        time.sleep(5)
+        return do_geocode(country,state,city,recursion=recursion + 1)
+
+
+citys = {}
+print "reading existed"
+with open('iploc2.csv') as fin:
+    spamreader = csv.reader(fin, delimiter=',', quotechar='"')
+    for eachline in spamreader:
+        citys[(eachline[0],eachline[1],eachline[2])] = (eachline[3],eachline[4])
+        
+print "reading all..."
+fout =  open('merge.csv','w') 
+fout.write("country,state,city,latitude,longitude,ip,mask\n")
+with open('dbip-city-2016-08.csv') as fin:
+    spamreader = csv.reader(fin, delimiter=',', quotechar='"')
+    for row in spamreader:
+        if (row[2],row[3],row[4]) not in citys:
+            #print row
+            loc = do_geocode(row[2],row[3],row[4])
+            time.sleep(2)
+            latitude = loc[0]
+            longitude = loc[1]
+            citys[(row[2],row[3],row[4])] = (latitude,longitude)
+        else:
+            latitude = citys[(row[2],row[3],row[4])][0]
+            longitude = citys[(row[2],row[3],row[4])][1]
+        try:
+            fout.write("\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n" % (row[2],row[3],row[4],latitude,longitude,row[0],row[1]))
+        except:
+            fout.write("\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\"\n" % (row[2],row[3],row[4],'None','None',row[0],row[1]))
+fout.close()
